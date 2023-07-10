76fa18b5dbd9b746d651445abb60f8d5d3f57432
StocktonManges <stocktonmanges@gmail.com>
2023-07-09 20:43:42 -0600

    buildGrid function completed.

--- part-1.js
+++ part-1.js
@@ -6,43 +6,66 @@ var cg = require('console-grid');
 // rs.keyInSelect(array, 'Gives a list of options to choose from and returns the index.');
 // rs.prompt('The user inputs whatever they want.');
 
-// rs.question('Press enter to begin... ');
+
+/***************************** TO DOS *****************************/
+// Choose the size of the board.
+// Place two ships on the board.
+// Player starts guessing locations.
+// Player is prompted to quit or play again.
+/******************************************************************/
+
+
+rs.question('Press enter to begin... ');
 
 const columnHeader = ['A','B','C','D','E','F','G','H','I','J'];
-
-function buildGrid(size) {
-  
-}
-
+const rows = [];
 const columns = [{
   "id": "name",
   "name": ""
-}, {
-  "id": "value",
-  "name": columnHeader[0]
-}, {
-"id": "value",
-"name": columnHeader[1]
-}, {
-"id": "value",
-"name": columnHeader[2]
 }];
 
-const rows = [{
-  "name": "1",
-  "value": ''
-}, {
-  "innerBorder": true
-}, {
-  "name": "2",
-  "value": ''
-}, {
-"innerBorder": true
-}, {
-  "name": "3",
-  "value": ''
-}];
+function buildGrid(size = 3) {
+  // Verifies the 'size' is between 3 and 10.
+  if (size > 10) {
+    size = 10;
+  } else if (size < 3) {
+    size = 3;
+  }
 
+  // Adds columns to the 'columns' list.
+  for (let i = 0; i < size; i++) {
+    columns.push( 
+      {
+        "id": "value",
+        "name": columnHeader[i]
+      },
+      )
+    }
+    
+  // Adds rows to the 'rows' list while checking for the last row.
+  for (let i = 0; i < size; i++) {
+    if (size - i === 1) {
+      rows.push(
+        {
+          "name": i + 1,
+          "value": ''
+        }
+      )
+    } else {
+      rows.push(
+        {
+          "name": i + 1,
+          "value": ''
+        },
+        { "innerBorder": true }
+      )
+    }
+  }
+}
+
+buildGrid();
+
+// Generate the grid in the console.
 cg({
   "columns": columns,
   "rows": rows,
