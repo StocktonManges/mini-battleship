854ba86ea67658db592387e525381ef0ab1192e5
StocktonManges <stocktonmanges@gmail.com>
2023-07-11 22:45:35 -0600

    gamePlay completed. Ready to merge with main branch.

--- part-1.js
+++ part-1.js
@@ -23,19 +23,19 @@ const ships = new Map();
 const allShipCoordinates = [];
 const shipCount = 2;
 
-const guesses = [];
+const strikeLocations = [];
 
 
-function buildGrid(gridSize = 3) {
+function buildGrid(dimensions = 3) {
   // Verifies the 'size' is between 3 and 10.
-  if (gridSize > 10) {
-    gridSize = 10;
-  } else if (gridSize < 3) {
-    gridSize = 3;
+  if (dimensions > 10) {
+    dimensions = 10;
+  } else if (dimensions < 3) {
+    dimensions = 3;
   }
   
   // Adds columns to the 'columns' list.
-  for (let i = 0; i < gridSize; i++) {
+  for (let i = 0; i < dimensions; i++) {
     columns.push( 
       {
         "id": "value",
@@ -44,37 +44,43 @@ function buildGrid(gridSize = 3) {
       )
     }
     
-    // Adds rows to the 'rows' list while checking for the last row.
-    for (let i = 0; i < gridSize; i++) {
-      if (gridSize - i === 1) {
+  // Adds rows to the 'rows' list while checking for the last row.
+  for (let i = 0; i < dimensions; i++) {
+    if (dimensions - i === 1) {
+      rows.push(
+        {
+          "name": i + 1,
+          "value": ''
+        }
+        )
+      } else {
         rows.push(
           {
             "name": i + 1,
             "value": ''
-          }
+          },
+          { "innerBorder": true }
           )
-        } else {
-          rows.push(
-            {
-              "name": i + 1,
-              "value": ''
-            },
-            { "innerBorder": true }
-            )
-          }
         }
+      }
+  
+  // Generate grid in the console.
+  cg({
+    "columns": columns,
+    "rows": rows,
+  });
 }
 
-function addShips(shipCount = 2) {
-  for (let i = 0; i < shipCount; i++) {
+function addShips(amount = 2) {
+  for (let i = 0; i < amount; i++) {
     ships.set(`ship${i + 1}`, '');
   }
 }
 
-function pickCoordinates(gridSize = 3) {
-  const coordinate1 = Math.floor(Math.random() * gridSize);
-  const coordinate2 = Math.floor(Math.random() * gridSize);
-  return `${columnHeader[coordinate1]}${coordinate2 + 1}`;
+function pickCoordinates(dimensions = 3) {
+  const coordinate1 = Math.floor(Math.random() * dimensions);
+  const coordinate2 = Math.floor(Math.random() * dimensions);
+  return `${(columnHeader[coordinate1]).toLowerCase()}${coordinate2 + 1}`;
 }
 
 function placeShips() {
@@ -91,19 +97,80 @@ function placeShips() {
 
 
 /***************************** GAME PLAY *****************************/
-// Player picks location.
-// Check if location is correct.
-// If correct, remove ship from object.
+// Debugging and safeguards
+// Check the strike input to make sure it's valid (verifyStrikeLocation() returns true or false).
+
+function strike() {
+  let strikeLocation = rs.question('Enter a location to strike. ').toLowerCase();
+  if (verifyStrikeLocation(strikeLocation)) {
+    for (let [ship, location] of ships) {
+      if (strikeLocation === location) {
+        ships.delete(ship);
+        console.log(`Hit. You have sunk a battleship. Remaining ships: ${ships.size}.`);
+        if (ships.size === 0) {
+          break;
+        } else {
+          strikeLocations.push(strikeLocation);
+          strike();
+        }
+      } else if (strikeLocations.includes(strikeLocation)) {
+        console.log('You have already picked this location. Miss!');
+        strikeLocations.push(strikeLocation);
+        strike();
+      } else {
+        console.log('You have missed!');
+        strikeLocations.push(strikeLocation);
+        strike();
+      }
+    }
+  } else {
+    console.log('That is not a valid location.');
+    strike();
+  }
+}
+
+// Split the strike location into an array and check if the letter is in
+// the 'columns' list and if the number is in the 'rows' list.
+function verifyStrikeLocation(str) {
+  newStr = str.toUpperCase();
+  const arr = [...newStr];
+  let rowCheck = false;
+  let columnCheck = false;
+  for (let i = 0; i < gridSize; i++) {
+    if (arr[0] == columns[`${i + 1}`].name) {
+      columnCheck = true;
+    }
+    if (arr[1] == rows[`${i * 2}`].name) {
+      rowCheck = true;
+    }
+  }
+  if (columnCheck && rowCheck && arr.length === 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function endGame() {
+  const playAgain = rs.keyInYN('You have destroyed all battleships. Would you like to play again?');
+  if (playAgain) {
+    startGame();
+  } else {
+    console.log(`
+    
+                              Thanks for playing!
+    `);
+  }
+}
+
+function startGame() {
+  addShips(shipCount);
+  buildGrid(gridSize);
+  placeShips();
+  strike();
+  endGame();
+}
+
 /******************************************************************/
 
-addShips(shipCount);
-// buildGrid(gridSize);
-placeShips();
-console.log(ships);
-console.log(allShipCoordinates);
-
-// Generate the grid in the console.
-// cg({
-//   "columns": columns,
-//   "rows": rows,
-// });
+startGame();
